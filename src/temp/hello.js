7ce02ad23895fd9ac03e2f3d6e870e1a84424b08
himdol <ihimchan746@gmail.com>
2023-02-26 12:38:42 +0900

    submit

--- src/temp/hello.js
+++ src/temp/hello.js
@@ -4,11 +4,7 @@ const loginButton = loginForm.querySelector("button");
 
 function onLoginBtnClick() {
   const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long.");
-  }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
