5e3d96c12ed3ec0aa3fcb1e6524226b34812852f
himdol <ihimchan746@gmail.com>
2023-02-26 13:52:23 +0900

    plus String of javascript and hidden form

--- src/temp/hello.js
+++ src/temp/hello.js
@@ -1,11 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  // const username = loginInput.value;
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  console.log(username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
