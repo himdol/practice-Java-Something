b6585341868c7dee9ac6542497926e8680ebfd05
himdol <ihimchan746@gmail.com>
2023-02-27 09:21:26 +0900

    "커넥션 개발 시작"

--- src/company/onboarding/src/main/ApplicationCompany.java
+++ src/company/onboarding/src/main/ApplicationCompany.java
@@ -1,11 +1,19 @@
 package company.onboarding.src.main;
 
+import company.onboarding.src.db.DAO;
 import company.onboarding.src.resouce.OnBoarding;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
 public class ApplicationCompany {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, SAXException {
 		OnBoarding on = new OnBoarding();
 		on.makeToExcelFileAndUpdate();
+		DAO dao = new DAO();
+		dao.getConnection();
 	}
 }
