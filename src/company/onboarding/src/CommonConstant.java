39f2a3308286a77c6b2ee4f5706b2ab3b37ebe2e
himdol <ihimchan746@gmail.com>
2023-02-27 18:10:39 +0900

    "칸 정리"

--- src/company/onboarding/src/CommonConstant.java
+++ src/company/onboarding/src/CommonConstant.java
@@ -1,10 +1,7 @@
 package company.onboarding.src;
 
 public class CommonConstant {
-
 	public final static String DEFAULT_DIR_PATH = System.getProperty("user.dir");
 	public final static String DB_INFO_XML_PATH = "/src/company/dbConnection.xml";
-
 	public final static int ZERO = 0;
-
 }
